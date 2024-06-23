cc21d641cc4941bec78b750f1e93d65d2c9c5787
Pavani Kadari <142245842+KadariPavani@users.noreply.github.com>
2024-06-23 11:54:26 +0530

    Delete Level1-Task1 directory

--- Level1-Task1/script.js
+++
@@ -1,9 +0,0 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-  const readMore = card.querySelector('.read-more');
-  readMore.addEventListener('click', () => {
-    card.classList.toggle('active');
-  });
-});
-
